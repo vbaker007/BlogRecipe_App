ca133deb4216cb753f8e7a3dce11787233eb1326
vbaker007 <vbaker007@hotmail.com>
2015-12-10 16:36:27 -0500

    Added CRUD Actions to Articles_Controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,36 +1,50 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
-
-  def show
-     @article = Article.find(params[:id])
-  end
-
+    http_basic_authenticate_with name: "mh", password: "secret",
+  except: [:index, :show]
+  
   def new
-     @article = Article.new
+    @article = Article.new
   end
-
+  
   def create
     @article = Article.new(article_params)
-  
-    @article.save
-    redirect_to @article
+    
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-
+  
   def edit
     @article = Article.find(params[:id])
   end
-
+  
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to(@book)
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
     else
-      render :edit
+      render 'edit'
     end
   end
-
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
