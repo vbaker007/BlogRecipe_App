927be558f451e834fee7e248f35c9c1a3a93e775
vbaker007 <vbaker007@hotmail.com>
2015-12-10 16:30:01 -0500

    Generated & Migrated Comment Model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,24 +11,29 @@ class ArticlesController < ApplicationController
      @article = Article.new
   end
 
- def create
-  @article = Article.new(article_params)
- 
-  @article.save
-  redirect_to @article
+  def create
+    @article = Article.new(article_params)
+  
+    @article.save
+    redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to(@book)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
 
-def update
-  @book = Book.find(params[:id])
-  if @book.update(book_params)
-    redirect_to(@book)
-  else
-    render :edit
-  end
-end
-
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
-end
