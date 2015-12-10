a8dc20d1785cb48bea1e9e440345145788441810
vbaker007 <vbaker007@hotmail.com>
2015-12-10 13:10:50 -0500

    added update method in Articles_Controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,16 @@ class ArticlesController < ApplicationController
   @article.save
   redirect_to @article
 end
- 
+
+def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    redirect_to(@book)
+  else
+    render :edit
+  end
+end
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
