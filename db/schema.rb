027a516d6edc5f1defe6b4a1ddfecf77f1047b42
vbaker007 <vbaker007@hotmail.com>
2015-12-12 21:30:13 -0500

    Create Chefs and Recipes Tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212201947) do
+ActiveRecord::Schema.define(version: 20151213020453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,27 @@ ActiveRecord::Schema.define(version: 20151212201947) do
     t.integer  "user_id"
   end
 
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
