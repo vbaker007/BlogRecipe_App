b7165626a3c32274140e68a4ab96e60e69ddb0c0
vbaker007 <vbaker007@hotmail.com>
2015-12-14 21:19:55 -0500

    Addd Validations to Recipe.rb model

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -9,4 +9,9 @@ class RecipeTest < ActiveSupport::TestCase
   test "recipe should be valid" do
     assert @recipe.valid?
   end
+
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
 end 
