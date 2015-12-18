456b44c2f4db5c24d6c6679235d38319ae5e731b
vbaker007 <vbaker007@hotmail.com>
2015-12-17 20:50:21 -0500

    Create Chef_ID (foreign key) & Migrate to Recipe Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213020453) do
+ActiveRecord::Schema.define(version: 20151218014518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151213020453) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
