7ec9b3014d1402cb422280cf1ef1cf59d917244c
vbaker007 <vbaker007@hotmail.com>
2015-12-17 20:41:12 -0500

    Added Valid Email Regex Association in chef.rb

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -31,4 +31,20 @@ class ChefTest < ActiveSupport::TestCase
     @chef.save
     assert_not dup_chef.valid?
   end
+
+  test "email validation should reject invalid addresses" do
+    valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au laura+joe@monk.com]
+    valid_addresses.each do |va|
+      @chef.email = va
+      assert @chef.valid?, '#{va.inspect} should be valid'
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_eee.org user.name@example. eee@i_am_.com foo@ee+aar.com]
+    invalid_addresses.each do |ia|
+      @chef.email = ia
+      assert_not @chef.valid?, '#{ia.inspect} should be invalid'
+    end
+  end 
 end
