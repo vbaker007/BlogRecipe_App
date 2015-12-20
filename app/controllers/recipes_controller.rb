2b6a2ed1d52e6cea26d96803262ae0d2a7c608b4
vbaker007 <vbaker007@hotmail.com>
2015-12-19 20:38:27 -0500

    Recipe_Controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,14 @@
 class RecipesController < ApplicationController 
-
+  
   def index
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
 end
