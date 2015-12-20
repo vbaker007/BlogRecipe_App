92f707e36a6ef07c032dad34d49133bbc7b81ba2
vbaker007 <vbaker007@hotmail.com>
2015-12-20 01:51:16 -0500

    Update Gravatar_for Method in helpers/application_helper.rb file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-
-  def gravatar_for(chef, options = {size: 80})
+  
+  def gravatar_for(chef, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
