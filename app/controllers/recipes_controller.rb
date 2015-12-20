b3e725331ed22574d0aaad34e0729693954e30eb
vbaker007 <vbaker007@hotmail.com>
2015-12-20 05:38:46 -0500

    Add Picture to Recipe_Params in Recipes_Controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,6 +47,6 @@ end
   private
   
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
+      params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
 end
