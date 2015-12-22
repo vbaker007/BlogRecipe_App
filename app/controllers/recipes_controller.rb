b05f0a77dfcf78c1f5c3c9e1ca66af830556f5f7
vbaker007 <vbaker007@hotmail.com>
2015-12-22 04:27:05 -0500

    Migration of create_likes; db/20151222092408_create_likes.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,6 +15,7 @@ class RecipesController < ApplicationController
   def create
      @recipe = Recipe.new(recipe_params)
      @recipe.chef = Chef.find(1)
+      
       if @recipe.save
         flash[:success] = "Your recipe was created successfully"
         redirect_to recipes_path
@@ -29,6 +30,7 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
+      
       if @recipe.update(recipe_params)
         flash[:success] = "Your recipe was updated successfully"
         redirect_to recipe_path(@recipe)
