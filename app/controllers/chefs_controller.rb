1388847871e094896f27522f6c23f1accff9f402
vbaker007 <vbaker007@hotmail.com>
2015-12-23 02:23:48 -0500

    Add Edit Method to Chefs_Controller.rb file

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,18 +1,30 @@
 class ChefsController < ApplicationController
 
   def new
-
+    @chef = Chef.new
   end
 
   def create
-
+    @chef = Chef.new(chef_params)
+    if @chef.save
+       flash[:success] = "Your account has been created successfully"
+       redirect_to recipes_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @chef = Chef.find(params[:id])
   end
 
   def update
 
   end
+
+  private
+
+    def chef_params
+      params.require(:chef).permit(:chefname, :email, :password)
+    end
 end
