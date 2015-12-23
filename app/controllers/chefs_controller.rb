a5fdf95942eddca81d9c9883bbc445c9d64f1a6d
vbaker007 <vbaker007@hotmail.com>
2015-12-23 02:32:24 -0500

    Add Update Method to Chefs_Controller.rb file

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -19,7 +19,13 @@ class ChefsController < ApplicationController
   end
 
   def update
-
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated successfully"
+      redirect_to recipes_path #To Do: Change to Show Chef Page
+    else 
+      render :edit
+    end
   end
 
   private
