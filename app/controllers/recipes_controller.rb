5889af59e515aaf7babae1be23a895b58079b417
vbaker007 <vbaker007@hotmail.com>
2015-12-25 07:52:29 -0500

    Add Before_action for Require_Same_User in Recipes_Controller.rb file

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController 
   before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
