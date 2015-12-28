7146918368fdc54e59f81554d57bacfc2a0fb4b5
vbaker007 <vbaker007@hotmail.com>
2015-12-28 07:13:01 -0500

    Fix Error in Ingredients_Controller.rb file

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,21 @@ class IngredientsController < ApplicationController
 
   def new
     @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ing_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient was created successfully"
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def ing_params
+    params.require(:ingredient).permit(:name)
+  end
 end
