c789a3e7d534f0aa662c66b4651010b608f92555
vbaker007 <vbaker007@hotmail.com>
2015-12-28 05:58:57 -0500

    Establish routes for Styles & Ingredients in Config/Routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   post '/login', to: "logins#create"
   get '/logout', to: "logins#destroy"
  
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
   
   resources :recipes do
     member do
