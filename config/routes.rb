981db5d8fa67bb0aac4a6d3ec165d246645ec637
vbaker007 <vbaker007@hotmail.com>
2015-12-29 11:41:48 -0500

    Add _form Partial for Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :reviews, only: [:new, :create, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
