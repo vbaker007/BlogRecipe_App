5e846b0060b15ea9cd9b4f8af6e5a0a670d1a6fb
vbaker007 <vbaker007@hotmail.com>
2016-01-01 21:43:52 -0500

    Modify spec/features/creating_recipe.rb file

--- spec/features/creating_recipe_spec.rb
+++ spec/features/creating_recipe_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
-
 RSpec.feature "Creating Recipes" do 
   scenario "A chef creates a new recipe" do
     visit "/"
-
-    click_link "New Recipe"
 
     fill_in "Name", with: "Creating first recipe"
     fill_in "Summary", with: "Lorem Ipsum"
     click_button "Create Recipe"
 
     expect(page).to have_content("Recipe has been successfully created")
-    expect(page.current_path).to eq(recipes_path)
+    expect(page.current_path).to eq(new_recipe_path)
   end
 end
