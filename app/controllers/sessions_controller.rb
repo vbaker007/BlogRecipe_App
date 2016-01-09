680e5d556ba710b0572aa6d96707d48ba0c44c64
vbaker007 <vbaker007@hotmail.com>
2016-01-08 21:01:42 -0500

    Twitter Sign in through app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   
   def create
+
     auth = request.env["omniauth.auth"]
+    #binding.pry
     chef = Chef.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Chef.create_with_omniauth(auth)
+    #chef = Chef.create(password)
     session[:chef_id] = chef.id
     flash[:success] = "Signed in!"
     redirect_to root_path
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,10 +4,10 @@ class Chef < ActiveRecord::Base
   has_many :reviews
   before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105 },
-                                    uniqueness: { case_sensitive: false },
-                                    format: { with: VALID_EMAIL_REGEX }
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, length: { maximum: 105 },
+                                    #uniqueness: { case_sensitive: false },
+                                    #format: { with: VALID_EMAIL_REGEX }
   has_secure_password 
 
   def self.create_with_omniauth(auth)
