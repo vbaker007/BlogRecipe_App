dfaf75de53b33f96100a2340e7f9c6e0bc794689
vbaker007 <vbaker007@hotmail.com>
2016-01-09 03:02:24 -0500

    Update Spec/Features/Showing_Recipe_Spec.rb File

--- spec/features/showing_recipe_spec.rb
+++ spec/features/showing_recipe_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Showing Recipe" do
 
   before do
-    @article = Article.create(name: "The first recipe", summary: "Body of first recipe", description: "this is a test to gage the successes and failure within this app")
+    @article = Article.create(name: "The first recipe", summary: "Summary of first recipe", description: "this is a test to gage the successes and failure within this app")
   end
 
   scenario "Display individual recipe" do
@@ -11,8 +11,9 @@ RSpec.feature "Showing Recipe" do
 
     click_link @recipe.name
 
-    expect(page).to have_content(@article.title)
-    expect(page).to have_content(@article.body)
+    expect(page).to have_content(@recipe.name)
+    expect(page).to have_content(@recipe.summary)
+    expect(page).to have_content(@recipe.description)
     expect(current_path).to eq(recipe_path(@recipe))
   end
 end
