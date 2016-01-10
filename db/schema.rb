cbda99ae2b7eea642fad88fe57caca29538cf9aa
vbaker007 <vbaker007@hotmail.com>
2016-01-09 21:47:11 -0500

    Add picture column to chefs_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107065502) do
+ActiveRecord::Schema.define(version: 20160110024512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160107065502) do
     t.string   "provider"
     t.string   "token"
     t.string   "secret"
+    t.string   "picture"
   end
 
   create_table "comments", force: :cascade do |t|
