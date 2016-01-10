7542db60673ce14574c7cd1ca4a32a2cda51b7a9
vbaker007 <vbaker007@hotmail.com>
2016-01-10 00:05:32 -0500

    Update Resize limit in Uploader/gravatar_uploader.rb file

--- app/uploaders/gravatar_uploader.rb
+++ app/uploaders/gravatar_uploader.rb
@@ -3,7 +3,7 @@
 class GravatarUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_limit: [600, 600]
+  process resize_to_limit: [200, 200]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
