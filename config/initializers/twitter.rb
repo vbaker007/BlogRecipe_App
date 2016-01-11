7023d9598a1b0f1d0f2b2664df093a25f8410aca
vbaker007 <vbaker007@hotmail.com>
2016-01-10 22:33:43 -0500

    Twitter

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,10 +1,7 @@
 client = Twitter::REST::Client.new do |config|
-  #config.twitter_api_key    = ENV['CONSUMER_KEY']
-  #config.twitter_api_secret   = ENV['CONSUMER_SECRET']
-  #config.oauth_token        = ENV["TWITTER_OAUTH_TOKEN"]
-  #config.oauth_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
-  config.consumer_key        = ENV['PZ2TyTLHigjVJJnNWAlrbQqPl']
-  config.consumer_secret     = ENV['5nsERRzArzlG3jc8X3cHKYLcW136uwEveVGkXdMa3UJPQInQco']
-  config.access_token        = ENV["2952709455-WcycfZJMixPx7LdirQHhgyIztziNbw56QPWuQLo"]
-  config.access_token_secret = ENV["1DpGEfZHYI92p5fywwx1jhsQTvXqvTkdOpIcTnGWLkgX8"]
+  config.twitter_api_key    = ENV['CONSUMER_KEY']
+  config.twitter_api_secret   = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV["TWITTER_OAUTH_TOKEN"]
+  config.oauth_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+ 
 end
