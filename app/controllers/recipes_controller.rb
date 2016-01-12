f4fe8a0b1589a85bea0699e12a0138677bd009c7
vbaker007 <vbaker007@hotmail.com>
2016-01-12 17:09:52 -0500

    Update Recipes Controller Index-Pagination Method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,7 @@ class RecipesController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
+    @recipes = Recipe.paginate(page: params[:page], per_page: 3)
   end
   
   def show
